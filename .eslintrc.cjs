49046b0eda34fd56ce09c196489134d0cb00da8f
Juan David <judaram98@gmail.com>
2023-10-04 17:50:27 -0600

    ✨feat:Eslint and Prettier integration and added styles

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,21 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    jest: true,
   },
-  extends: ['standard-with-typescript', 'plugin:react/recommended'],
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json'
+    project: './tsconfig.json',
   },
   plugins: ['react'],
   rules: {
-    'react/react-in-jsx-scope': 'off'
-  }
-}
+    'react/react-in-jsx-scope': 'off',
+  },
+};
